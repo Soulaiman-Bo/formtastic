64439fdc2b2efbf3b354379911aba20779591bdc
soulaiman-bo <bouhlal.soulaiman@gmail.com>
2024-04-12 17:04:40 +0000

    i did setup the dashboard

---
+++ src/components/DashboardHeader.tsx
@@ -0,0 +1,31 @@
+import NewFormButton from "./NewFormButton";
+import { Search } from "lucide-react";
+import { Folder } from "lucide-react";
+
+const DashboardHeader = () => {
+  return (
+    <div className="hidden md:flex justify-between mb-5 border-b border-gray-300 pb-4 px-7 mt-5">
+      <div className="text-lg font-bold leading-7 flex items-center text-gray-700">
+        <Folder className="h-7 w-7 mr-3" fill="#d1d5db" strokeWidth={0} />
+
+        <div
+          className="max-w-[400px] truncate"
+          data-cy="home-page-header-workspace-name"
+        >
+          Home
+        </div>
+      </div>
+
+      <div className="flex items-center">
+        <NewFormButton />
+        <div className="inline-block w-5 h-5 ml-5">
+          <button>
+            <Search strokeWidth={3} color="#94a3b8" className="h-4 w-4" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardHeader;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,16 @@
+import DashboardHeader from "@/components/DashboardHeader";
+import FormCardSkeleton from "@/components/FormCardSkeleton";
+import FormCards from "@/components/FormCards";
+import Sidebar from "@/components/Sidebar";
 import { isAuthenticated } from "@/lib/auth";
 import { createFileRoute, redirect } from "@tanstack/react-router";
+import { Suspense } from "react";
 
 export const Route = createFileRoute("/")({
   beforeLoad: () => {
     if (!isAuthenticated()) {
       throw redirect({
-        to: '/auth/login'
+        to: "/auth/login",
       });
     }
   },
@@ -14,8 +19,24 @@ export const Route = createFileRoute("/")({
 
 function Index() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
-    </div>
+    <>
+      <Sidebar />
+      <div className="md:pl-72 flex flex-col h-screen">
+        <main className="flex-1 flex flex-col bg-gray-50">
+          <DashboardHeader />
+          <div className="flex-grow overflow-y-auto mx-6 md:mx-12 my-2 md:my-3">
+            <h2 className=" py-2 w-fit rounded-full font-semibold hover:bg-blue-100 hover:text-blue-500 px-4 m-1 cursor-pointer text-gray-400">
+              All forms
+            </h2>
+
+            
+
+            <Suspense fallback={<FormCardSkeleton />}>
+              <FormCards />
+            </Suspense>
+          </div>
+        </main>
+      </div>
+    </>
   );
 }
