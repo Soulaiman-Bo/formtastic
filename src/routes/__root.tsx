dcf81cfc9c903d6bbc89919c3e2f4d43c007843e
soulaiman-bo <bouhlal.soulaiman@gmail.com>
2024-04-12 17:05:43 +0000

    i did setup the loading animation on cards and workspaces

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,26 +1,9 @@
-import LogoutButton from "@/components/LogoutButton";
-import { isAuthenticated } from "@/lib/auth";
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="flex items-center justify-between px-8">
-        <ul className="flex gap-4 p-5">
-          <li>
-            <Link to="/auth/login">login</Link>
-          </li>
-          <li>
-            <Link to="/auth/signup">signup</Link>
-          </li>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-        </ul>
-        {isAuthenticated() && <LogoutButton />}
-      </div>
-
       <Outlet />
       <TanStackRouterDevtools />
     </>
