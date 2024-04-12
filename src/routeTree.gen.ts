cc14073adf369243b87cefa23086372c28b1f5ad
soulaiman-bo <bouhlal.soulaiman@gmail.com>
2024-04-12 13:48:51 +0000

    i changed routes

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -11,24 +11,16 @@
 // Import Routes
 
 import { Route as rootRoute } from './routes/__root'
-import { Route as SignupImport } from './routes/signup'
-import { Route as LoginImport } from './routes/login'
 import { Route as ErrorImport } from './routes/error'
 import { Route as IndexImport } from './routes/index'
+import { Route as AuthSignupImport } from './routes/auth/signup'
+import { Route as AuthResetpasswordImport } from './routes/auth/resetpassword'
+import { Route as AuthLoginImport } from './routes/auth/login'
+import { Route as AuthForgetPasswordImport } from './routes/auth/ForgetPassword'
 import { Route as StudioPostIdIndexImport } from './routes/studio/$postId/index'
 import { Route as StudioPostIdEditImport } from './routes/studio/$postId/edit'
 
 // Create/Update Routes
-
-const SignupRoute = SignupImport.update({
-  path: '/signup',
-  getParentRoute: () => rootRoute,
-} as any)
-
-const LoginRoute = LoginImport.update({
-  path: '/login',
-  getParentRoute: () => rootRoute,
-} as any)
 
 const ErrorRoute = ErrorImport.update({
   path: '/error',
@@ -37,6 +29,26 @@ const ErrorRoute = ErrorImport.update({
 
 const IndexRoute = IndexImport.update({
   path: '/',
+  getParentRoute: () => rootRoute,
+} as any)
+
+const AuthSignupRoute = AuthSignupImport.update({
+  path: '/auth/signup',
+  getParentRoute: () => rootRoute,
+} as any)
+
+const AuthResetpasswordRoute = AuthResetpasswordImport.update({
+  path: '/auth/resetpassword',
+  getParentRoute: () => rootRoute,
+} as any)
+
+const AuthLoginRoute = AuthLoginImport.update({
+  path: '/auth/login',
+  getParentRoute: () => rootRoute,
+} as any)
+
+const AuthForgetPasswordRoute = AuthForgetPasswordImport.update({
+  path: '/auth/ForgetPassword',
   getParentRoute: () => rootRoute,
 } as any)
 
@@ -62,12 +74,20 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof ErrorImport
       parentRoute: typeof rootRoute
     }
-    '/login': {
-      preLoaderRoute: typeof LoginImport
+    '/auth/ForgetPassword': {
+      preLoaderRoute: typeof AuthForgetPasswordImport
       parentRoute: typeof rootRoute
     }
-    '/signup': {
-      preLoaderRoute: typeof SignupImport
+    '/auth/login': {
+      preLoaderRoute: typeof AuthLoginImport
+      parentRoute: typeof rootRoute
+    }
+    '/auth/resetpassword': {
+      preLoaderRoute: typeof AuthResetpasswordImport
+      parentRoute: typeof rootRoute
+    }
+    '/auth/signup': {
+      preLoaderRoute: typeof AuthSignupImport
       parentRoute: typeof rootRoute
     }
     '/studio/$postId/edit': {
@@ -86,8 +106,10 @@ declare module '@tanstack/react-router' {
 export const routeTree = rootRoute.addChildren([
   IndexRoute,
   ErrorRoute,
-  LoginRoute,
-  SignupRoute,
+  AuthForgetPasswordRoute,
+  AuthLoginRoute,
+  AuthResetpasswordRoute,
+  AuthSignupRoute,
   StudioPostIdEditRoute,
   StudioPostIdIndexRoute,
 ])
