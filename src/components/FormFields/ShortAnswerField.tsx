d2274396b651417424373f0fed8edb6296dd75aa
soulaiman-bo <bouhlal.soulaiman@gmail.com>
2024-04-17 22:42:47 +0100

    i created properties component of short text field

--- src/components/FormFields/ShortAnswerField.tsx
+++ src/components/FormFields/ShortAnswerField.tsx
@@ -5,22 +5,32 @@ import {
   FormElementInstance,
 } from "../FormElements";
 import { Input } from "../ui/input";
-import { Label } from "../ui/label";
 import { Switch } from "../ui/switch";
-import { Separator } from "../ui/separator";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form";
+import usePlayground from "@/hooks/usePlayground";
 
 const type: ElementsType = "ShortAnswerField";
 
 const extraAttributes = {
-  label: "your question here",
+  question: "Write you Question here.",
   helperText: "Helper Text",
   required: false,
   placeHolder: "Answer Here ...",
-};
+}; // these meant to go in main component
+
+const propertiesSchema = z.object({
+  question: z.string().min(2).max(50),
+  helperText: z.string().max(200),
+  required: z.boolean().default(false),
+  placeHolder: z.string().max(50),
+});
 
 export const ShortAnswerFieldFormElement: FormElement = {
   type,
-
   construct: (id: string) => ({
     id,
     type,
@@ -47,12 +57,12 @@ function MainComponent({
   elementInstance: FormElementInstance;
 }) {
   const element = elementInstance as CustomInstance;
-  const { helperText, placeHolder, label } = element.extraAttributes;
+  const { helperText, placeHolder, question, required } = element.extraAttributes;
 
   return (
     <div className=" w-full">
       <p className="font-inter text-base font-medium  text-gray-600 my-2">
-        {label}
+        {question}{required && (<span className="text-red-500">*</span>)}
       </p>
       <Input
         className="shadow-sm block w-full mb-3 rounded-md placeholder:text-gray-300 border-gray-300 text-base text-gray-500 font-medium h-11"
@@ -65,74 +75,146 @@ function MainComponent({
   );
 }
 
-function PropertiesComponent({ elementInstance }: { elementInstance: FormElementInstance }) {
-  
-  // const element = elementInstance as CustomInstance;
+type propertiesFormSchemaType = z.infer<typeof propertiesSchema>;
 
+function PropertiesComponent({
+  elementInstance,
+}: {
+  elementInstance: FormElementInstance;
+}) {
+  const element = elementInstance as CustomInstance;
+  const { helperText, placeHolder, question, required } =
+    element.extraAttributes;
+  const { updateElement } = usePlayground();
 
- 
+  const form = useForm<propertiesFormSchemaType>({
+    resolver: zodResolver(propertiesSchema),
+    defaultValues: {
+      question: question,
+      helperText: helperText,
+      required: required,
+      placeHolder: placeHolder,
+    },
+  });
+
+  useEffect(() => {
+    form.reset(element.extraAttributes);
+  }, [element, form]);
+
+  function applyChanges(values: propertiesFormSchemaType) {
+    const { question, helperText, placeHolder, required } = values;
+    updateElement(element.id, {
+      ...element,
+      extraAttributes: {
+        question,
+        helperText,
+        placeHolder,
+        required,
+      },
+    });
+  }
+
   return (
     <div className="flex flex-col gap-2 px-4">
       <div>
         <p className="font-inter font-semibold text-slate-500">Basic</p>
-        <div className="my-4 flex flex-col gap-8">
-          <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label
-              className="font-inter mb-1 font-light text-slate-600"
-              htmlFor="email"
-            >
-              Question
-            </Label>
-            <Input
-              className="placeholder:text-gray-300"
-              type="text"
-              id="label"
-              placeholder="Question Here ..."
-            />
-          </div>
 
-          <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label
-              className="font-inter mb-1 font-light text-slate-600"
-              htmlFor="email"
-            >
-              Helper Text
-            </Label>
-            <Input
-              className="placeholder:text-gray-300"
-              type="text"
-              id="label"
-              placeholder="Question Here ..."
-            />
-          </div>
+        <Form {...form}>
+          <form onBlur={form.handleSubmit(applyChanges)}>
+            <div className="my-4 flex flex-col gap-8">
+              <FormField
+                control={form.control}
+                name="question"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="grid w-full max-w-sm items-center gap-1.5">
+                      <FormLabel className="font-inter mb-1 font-light text-slate-600">
+                        Question
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          className="placeholder:text-gray-300"
+                          {...field}
+                          onKeyDown={(e) => {
+                            if (e.key === "Enter") e.currentTarget.blur();
+                          }}
+                        />
+                      </FormControl>
+                    </div>
+                  </FormItem>
+                )}
+              />
 
-          <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label
-              className="font-inter mb-1 font-light text-slate-600"
-              htmlFor="email"
-            >
-              Default value
-            </Label>
-            <Input
-              className="placeholder:text-gray-300"
-              type="text"
-              id="label"
-            />
-          </div>
+              <FormField
+                control={form.control}
+                name="helperText"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="grid w-full max-w-sm items-center gap-1.5">
+                      <FormLabel className="font-inter mb-1 font-light text-slate-600">
+                        Helper Text
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          className="placeholder:text-gray-300"
+                          {...field}
+                          onKeyDown={(e) => {
+                            if (e.key === "Enter") e.currentTarget.blur();
+                          }}
+                        />
+                      </FormControl>
+                    </div>
+                  </FormItem>
+                )}
+              />
 
-          <div className="flex px-3 items-center justify-between space-x-2">
-            <Label
-              className="font-inter mb-1 font-light text-slate-600"
-              htmlFor="required"
-            >
-              Required
-            </Label>
+              <FormField
+                control={form.control}
+                name="placeHolder"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="grid w-full max-w-sm items-center gap-1.5">
+                      <FormLabel className="font-inter mb-1 font-light text-slate-600">
+                        PlaceHolder
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          className="placeholder:text-gray-300"
+                          {...field}
+                          onKeyDown={(e) => {
+                            if (e.key === "Enter") e.currentTarget.blur();
+                          }}
+                        />
+                      </FormControl>
+                    </div>
+                  </FormItem>
+                )}
+              />
 
-            <Switch id="required" />
-          </div>
-        </div>
+              <FormField
+                control={form.control}
+                name="required"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="flex px-3 items-center justify-between space-x-2">
+                      <FormLabel className="font-inter mb-1 font-light text-slate-600">
+                        PlaceHolder
+                      </FormLabel>
+                      <FormControl>
+                        <Switch
+                          checked={field.value}
+                          onCheckedChange={field.onChange}
+                        />
+                      </FormControl>
+                    </div>
+                  </FormItem>
+                )}
+              />
+            </div>
+          </form>
+        </Form>
       </div>
-      <Separator />
+      {/* <Separator />
       <div>
         <p className="font-inter font-semibold text-slate-500">Validation</p>
         <div className="my-4 flex flex-col gap-8">
@@ -180,7 +262,7 @@ function PropertiesComponent({ elementInstance }: { elementInstance: FormElement
             />
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
