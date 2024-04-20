67696b81eaec44755bc28ebbedb8feec83a97a5e
soulaiman-bo <bouhlal.soulaiman@gmail.com>
2024-04-20 15:51:04 +0100

    re orgnized components

--- src/components/FormFields/shortAnserField/PropertiesComponent.tsx
+++ src/components/FormFields/shortAnserField/PropertiesComponent.tsx
@@ -1,26 +1,17 @@
-import { AlignCenter } from "lucide-react";
+import { FormElementInstance } from "@/components/FormElements";
 import {
-  ElementsType,
-  FormElement,
-  FormElementInstance,
-} from "../FormElements";
-import { Input } from "../ui/input";
-import { Switch } from "../ui/switch";
-import { z } from "zod";
-import { useForm } from "react-hook-form";
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Switch } from "@/components/ui/switch";
+import usePlayground from "@/hooks/usePlayground";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useEffect } from "react";
-import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form";
-import usePlayground from "@/hooks/usePlayground";
-
-const type: ElementsType = "ShortAnswerField";
-
-const extraAttributes = {
-  question: "Write you Question here.",
-  helperText: "Helper Text",
-  required: false,
-  placeHolder: "Answer Here ...",
-}; // these meant to go in main component
+import { useForm, FormProvider } from "react-hook-form";
+import { z } from "zod";
 
 const propertiesSchema = z.object({
   question: z.string().min(2).max(50),
@@ -29,60 +20,26 @@ const propertiesSchema = z.object({
   placeHolder: z.string().max(50),
 });
 
-export const ShortAnswerFieldFormElement: FormElement = {
-  type,
-  construct: (id: string) => ({
-    id,
-    type,
-    extraAttributes,
-  }),
-
-  mainComponent: MainComponent,
-  previewComponent: () => <div>preview Component</div>,
-  propertiesComponent: PropertiesComponent,
-  draggableCardComponent: {
-    icon: AlignCenter,
-    label: "Short Answer",
-    color: "green-500",
-  },
-};
+type propertiesFormSchemaType = z.infer<typeof propertiesSchema>;
 
 type CustomInstance = FormElementInstance & {
   extraAttributes: typeof extraAttributes;
 };
 
-function MainComponent({
+const extraAttributes = {
+  question: "Write you Question here.",
+  helperText: "Helper Text",
+  required: false,
+  placeHolder: "Answer Here ...",
+}; // these meant to go in main component
+
+export default function PropertiesComponent({
   elementInstance,
 }: {
   elementInstance: FormElementInstance;
 }) {
   const element = elementInstance as CustomInstance;
-  const { helperText, placeHolder, question, required } = element.extraAttributes;
 
-  return (
-    <div className=" w-full">
-      <p className="font-inter text-base font-medium  text-gray-600 my-2">
-        {question}{required && (<span className="text-red-500">*</span>)}
-      </p>
-      <Input
-        className="shadow-sm block w-full mb-3 rounded-md placeholder:text-gray-300 border-gray-300 text-base text-gray-500 font-medium h-11"
-        placeholder={placeHolder}
-      />
-      {helperText && (
-        <p className="pl-3 text-gray-400 text-sm mb-2">{helperText}</p>
-      )}
-    </div>
-  );
-}
-
-type propertiesFormSchemaType = z.infer<typeof propertiesSchema>;
-
-function PropertiesComponent({
-  elementInstance,
-}: {
-  elementInstance: FormElementInstance;
-}) {
-  const element = elementInstance as CustomInstance;
   const { helperText, placeHolder, question, required } =
     element.extraAttributes;
   const { updateElement } = usePlayground();
@@ -97,9 +54,23 @@ function PropertiesComponent({
     },
   });
 
+//   const values = form.watch();
+
+  console.log("properties component rendered");
+  
+
   useEffect(() => {
+    const subscription = form.watch(() => {
+      form.handleSubmit(applyChanges)();
+    });
+
     form.reset(element.extraAttributes);
-  }, [element, form]);
+
+
+    return () => subscription.unsubscribe();
+
+
+  }, [form]);
 
   function applyChanges(values: propertiesFormSchemaType) {
     const { question, helperText, placeHolder, required } = values;
@@ -119,8 +90,8 @@ function PropertiesComponent({
       <div>
         <p className="font-inter font-semibold text-slate-500">Basic</p>
 
-        <Form {...form}>
-          <form onBlur={form.handleSubmit(applyChanges)}>
+        <FormProvider {...form}>
+          <form >
             <div className="my-4 flex flex-col gap-8">
               <FormField
                 control={form.control}
@@ -212,57 +183,8 @@ function PropertiesComponent({
               />
             </div>
           </form>
-        </Form>
+        </FormProvider>
       </div>
-      {/* <Separator />
-      <div>
-        <p className="font-inter font-semibold text-slate-500">Validation</p>
-        <div className="my-4 flex flex-col gap-8">
-          <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label
-              className="font-inter mb-1 font-light text-slate-600"
-              htmlFor="email"
-            >
-              Min Length
-            </Label>
-            <Input
-              className="placeholder:text-gray-300"
-              type="text"
-              id="label"
-              placeholder="Question Here ..."
-            />
-          </div>
-
-          <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label
-              className="font-inter mb-1 font-light text-slate-600"
-              htmlFor="email"
-            >
-              Max Length
-            </Label>
-            <Input
-              className="placeholder:text-gray-300"
-              type="text"
-              id="label"
-              placeholder="Question Here ..."
-            />
-          </div>
-
-          <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label
-              className="font-inter mb-1 font-light text-slate-600"
-              htmlFor="email"
-            >
-              Error message
-            </Label>
-            <Input
-              className="placeholder:text-gray-300"
-              type="text"
-              id="label"
-            />
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 }
---
+++ src/components/FormFields/shortAnserField/ShortAnswerField.tsx
@@ -0,0 +1,91 @@
+import { AlignCenter } from "lucide-react";
+import {
+  ElementsType,
+  FormElement,
+} from "../../FormElements";
+
+import MainComponent from "./MainComponent";
+import PropertiesComponent from "./PropertiesComponent";
+
+const type: ElementsType = "ShortAnswerField";
+
+const extraAttributes = {
+  question: "Write you Question here.",
+  helperText: "Helper Text",
+  required: false,
+  placeHolder: "Answer Here ...",
+}; // these meant to go in main component
+
+
+
+export const ShortAnswerFieldFormElement: FormElement = {
+  type,
+  construct: (id: string) => ({
+    id,
+    type,
+    extraAttributes,
+  }),
+
+  mainComponent: MainComponent,
+  previewComponent: () => <div>preview Component</div>,
+  propertiesComponent: PropertiesComponent,
+  draggableCardComponent: {
+    icon: AlignCenter,
+    label: "Short Answer",
+    color: "green-500",
+  },
+};
+
+
+
+{
+  /* <Separator />
+      <div>
+        <p className="font-inter font-semibold text-slate-500">Validation</p>
+        <div className="my-4 flex flex-col gap-8">
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label
+              className="font-inter mb-1 font-light text-slate-600"
+              htmlFor="email"
+            >
+              Min Length
+            </Label>
+            <Input
+              className="placeholder:text-gray-300"
+              type="text"
+              id="label"
+              placeholder="Question Here ..."
+            />
+          </div>
+
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label
+              className="font-inter mb-1 font-light text-slate-600"
+              htmlFor="email"
+            >
+              Max Length
+            </Label>
+            <Input
+              className="placeholder:text-gray-300"
+              type="text"
+              id="label"
+              placeholder="Question Here ..."
+            />
+          </div>
+
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label
+              className="font-inter mb-1 font-light text-slate-600"
+              htmlFor="email"
+            >
+              Error message
+            </Label>
+            <Input
+              className="placeholder:text-gray-300"
+              type="text"
+              id="label"
+            />
+          </div>
+        </div>
+      </div> */
+}
